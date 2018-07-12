7b56412176ee4f9d447c12765e8805bc3b674592
xinyu-git <925266768@qq.com>
2018-07-12 18:29:22 +0800

    ceshi

--- src/views/order/orderList.vue
+++ src/views/order/orderList.vue
@@ -27,6 +27,7 @@
                     <b-col lg="12">
                         <b-table  striped hover border :items="orderData" :fields="fieldsOrder">
                             <template slot="add_time" slot-scope="time">
+                                {{time}}
                                 {{time.item.add_time}}
                             </template>
                             <template slot="order_status_text" slot-scope="data">
